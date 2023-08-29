6bb3649384f814af50c2fecf4ac13d922730225b
RenandRosa <142436623+RenandRosa@users.noreply.github.com>
2023-08-29 15:28:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,100 @@
+//A
+//1.
+//a) undefined
+//b) null
+//c) 11
+//d) 3
+//e) (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f) 9
+
+//2
+//SUBI NUM ÔNIBUS EM MIRRICOS 27
+
+//B
+//1
+/*
+let seuNome = prompt('Qual o seu nome');
+let seuEmail = prompt('Qual o seu email');
+
+console.log(seuNome);
+console.log(seuEmail);
+console.log('O e-mail ', seuEmail, 'foi cadastrado com sucesso. Seja bem-vinda(o),', seuNome,'!');
+*/
+
+//2.
+/*
+let comidas = ['Pizza' ,'Hamburguer' ,'Lasanha' ,'Churrasco' ,'Sorvete'];
+
+console.log(comidas[0]);
+console.log(comidas[1]);
+console.log(comidas[2]);
+console.log(comidas[3]);
+console.log(comidas[4]);
+
+console.log('Essas são minhas comidas favoritas ');
+console.log(comidas[0]);
+console.log(comidas[1]);
+console.log(comidas[2]);
+console.log(comidas[3]);
+console.log(comidas[4]);
+
+let suaComida = prompt('Qual a sua comida favorita?');
+
+console.log(suaComida.replaceAll(suaComida, comidas[1]));
+*/
+
+//3.
+/*
+let listaDeTarefas = [];
+
+console.log(listaDeTarefas);
+
+let tarefaUm = prompt('Digite uma tarefa para realizar ao dia');
+let tarefaDois = prompt('Digite uma tarefa para realizar ao dia');
+let tarefaTrês = prompt('Digite uma tarefa para realizar ao dia');
+
+console.log(tarefaUm);
+console.log(tarefaDois);
+console.log(tarefaTrês);
+
+listaDeTarefas.push(tarefaUm);
+listaDeTarefas.push(tarefaDois);
+listaDeTarefas.push(tarefaTrês);
+
+console.log(listaDeTarefas);
+console.log(tarefaUm[0]);
+console.log(tarefaUm[1]);
+console.log(tarefaUm[2]);
+
+let tarefasRealizadas = prompt('Tarefas realizadas ');
+listaDeTarefas.splice(tarefasRealizadas, 1);
+
+console.log(listaDeTarefas);
+
+listaDeTarefas.pop[tarefasRealizadas];
+*/
+
+//Desafios
+//1
+/*
+let frase = 'Amanhã não vou na aula';
+
+console.log(frase);
+
+let RecebaFrase = [frase.replaceAll(' ', '')];
+
+console.log(RecebaFrase);
+*/
+
+//2
+let frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
+
+/*frutas.includes('Abacaxi');*/
+
+console.log(frutas.indexOf('Abacaxi'));
